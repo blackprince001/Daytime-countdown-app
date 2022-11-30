9723c4c232f3ca779406061a2482bac41e891c30
Blackprincex <appiahboaduprince@gmail.com>
2022-11-30 20:32:41 +0000

    upd: change to design to app inspired by StopWatch app include textual docs
    docs: code documented

--- daytime_countdown_app/main.py
+++ daytime_countdown_app/main.py
@@ -1,36 +1,63 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Button, Label
+from textual.widgets import Static, Header, Footer
 
-import datetime
+from textual.reactive import reactive
+from textual.containers import Container
+
+import time
 
 
-class QuestionApp(App[str]):
+class Timer(Static):
+    SECS_IN_DAY = 86_400
+    remaining_time = SECS_IN_DAY - (
+        time.localtime().tm_hour * 3600
+        + time.localtime().tm_min * 60
+        + time.localtime().tm_sec
+    )
+    start_time = reactive(time.time)
+    time = reactive(0.0)
 
-    @staticmethod
-    def _lim_time(now: datetime) -> int:
-        dtime = 86_400
-        elapsed_seconds = (now.hour * 3600) + (now.minute * 60) + (now.second)
-        return dtime - elapsed_seconds
+    def update_time(self) -> None:
+        """updates the `time` attribute of the `Timer` class Static Widget."""
+        self.time = self.remaining_time - (time.time() - self.start_time)
+
+    def watch_time(self, time: float) -> None:
+        """gets called when updating the `time` attribute."""
+        self.update(f"You have {time:05.2f} seconds left in your day.")
+
+    def on_mount(self) -> None:
+        """gets called when the `Timer` class is called"""
+        self.update_timer = self.set_interval(1 / 60, self.update_time, pause=False)
+
+    def check_time(self) -> None:
+        """sets the `start_time` attribute to current time on system."""
+        self.start_time = time.time()
+
+    def quit(self) -> None:
+        """quits"""
+        self.exit()
+
+
+class TimeDisplay(Static):
+    """Display the Seconds left using data from the `Timer` class."""
+    def compose(self) -> None:
+        yield Timer(classes="seconds")
+
+
+class DaytimeCountdownApp(App):
+
+    CSS_PATH = "style-main.css"
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
     def compose(self) -> ComposeResult:
-        tnow = datetime.datetime.now()
-        tval = self._lim_time(now=tnow)
-        
-        # yield Time(msg=tval)
-        self.box = Label(f"Seconds left: {next(tval)}")
-        self.box.styles.background = "red"
-        self.box.styles.color = "black"
-        self.box.styles.padding = (1, 2)
+        yield Header()
+        yield Footer()
+        yield Container(TimeDisplay())
 
-        yield self.box if tval != 0 else self.box
+    def action_toggle_dark(self) -> None:
+        self.dark = not self.dark
 
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        self.exit(event.button.id)
-
-
-    def on_mount(self):
-        self.box.styles.animate("opacity", value=0.0, duration=2.0)
 
 if __name__ == "__main__":
-    app = QuestionApp()
+    app = DaytimeCountdownApp()
     app.run()
